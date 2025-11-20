b792402307cab8749a6280c1b4b0f8ced7170673
Sarasofcreator <taverasara101@gmail.com>
2025-11-19 20:57:43 -0500

    Create Sarasofcreator.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Sarasofcreator.py
@@ -0,0 +1,48 @@
+"""
+ EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje
+ """
+
+# https://python.org
+
+# Comentario en una linea 
+
+"""
+Esto es un comentario
+de varias 
+lineas,y lo puedo cerrar 
+entre comillas.
+"""
+
+#Variable y constante 
+
+My_variable = "Mi variable"
+CONSTANTE = "Esta es una constante por convención"    # Si se escribe en mayuscula de entiende que es una constante, se usa a convención
+
+#Tipos de datos primitivos 
+
+'Cadena de texto
+my_str = "string"
+
+'#Enteros
+my_int = 1
+'#Flotantes
+mi_float = 1.2
+
+'Booleanos
+my_bool = True
+my_bool2 = False
+
+'String
+my_str = "Esto es un string"
+my_otherstring = "Mi cadena de texto 
+
+print("Hola, Python")
+print(type (my_int))
